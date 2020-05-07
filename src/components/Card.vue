57606e0af7d1d82003c045823e832975f05ac5a9
Raul-Octavina Albu <alburaul@gmil.com>
2020-05-07 10:45:16 +0200

    four items added

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,31 +1,92 @@
 <template>
-  <article class="shadow">
-    <a href="#">
+  <article class="bigCard">
+    <section class="shadow card">
       <div class="img-container">
-        <div class="project-image"></div>
-        <h4>name of project</h4>
+        <div class="project-image">
+          <img :src="image" />
+        </div>
+        <h4>{{name}}</h4>
       </div>
-      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p class="card-text">{{ dSmall }}</p>
       <a class="flex">
-        <button class="read-more">Read more</button>
+        <button @click="showCard = !showCard" class="read-more">Read more</button>
       </a>
-    </a>
+    </section>
+    <section v-show="showCard" class="description">
+      <h4>{{ name }}</h4>
+      <p>{{ description }}</p>
+      <p>{{ dTwo }}</p>
+      <div class="contact bottom">
+        <a :href="sourceLink" target="blank">
+          <button>Source</button>
+        </a>
+        <a :href="activeLink" target="blank">
+          <button>Active website</button>
+        </a>
+      </div>
+    </section>
   </article>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      showCard: false
+    };
+  },
+  methods: {},
+  props: {
+    name: String,
+    dSmall: String,
+    description: String,
+    dTwo: String,
+    sourceLink: String,
+    activeLink: String,
+    image: String
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 article {
+  max-width: 100%;
+  display: flex;
+}
+.description {
+  flex-grow: 1;
+  padding: 20px 40px;
+  margin: 20px 0;
+  position: relative;
+  h4 {
+    margin-bottom: 40px;
+  }
+  p {
+    margin-bottom: 20px;
+    font-size: $font-size-normal-tablet;
+  }
+}
+
+.bottom {
+  position: absolute;
+  bottom: 40px;
+  button {
+    &:nth-child(1) {
+      margin-right: 20px;
+    }
+  }
+}
+
+.card {
   height: 500px;
+  min-width: 350px;
   width: 350px;
   background: $Gravity---Sand-Creatures-II-5-rgba;
   padding: 20px 40px;
   transition: all 500ms ease-in-out;
   border: 3px solid white;
   margin: 20px;
+  position: relative;
 
   &:hover {
     transform: translateY(3px);
@@ -50,9 +111,12 @@ a {
   justify-content: center;
   flex-wrap: wrap;
   padding: 10px 0;
+  img {
+    width: 100%;
+    height: 100%;
+  }
 }
 .project-image {
-  background: url("../assets/mobile first responsive app.png");
   background-position: center;
   background-size: cover;
   width: 100%;
@@ -92,17 +156,51 @@ button {
 }
 .read-more {
   z-index: 5;
+  position: absolute;
+  bottom: 40px;
 }
 
-@media screen and (max-width: 600px) {
+@media screen and (max-width: 1100px) {
   article {
-    height: 430px;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .description {
+    flex-grow: 1;
+    padding: 20px 0;
+    margin: 20px 0;
+    position: relative;
+    h4 {
+      margin-bottom: 40px;
+    }
+    p {
+      margin-bottom: 20px;
+      font-size: $font-size-normal-tablet;
+    }
+  }
+  .bottom {
+    position: initial;
+    bottom: initial;
+    button {
+      &:nth-child(1) {
+        margin: 20px 20px 20px 0;
+      }
+    }
+  }
+  .card {
+    height: 420px;
+    min-width: 320px;
     width: 320px;
     background: $Gravity---Sand-Creatures-II-5-rgba;
-    padding: 20px 10px;
+    padding: 20px 40px;
     transition: all 500ms ease-in-out;
-    border: 2px solid white;
-    margin: none;
+    border: 3px solid white;
+    margin: 20px 0;
+    position: relative;
+
+    &:hover {
+      transform: translateY(3px);
+    }
   }
 }
 </style>
--- src/components/projects-container.vue
+++ src/components/projects-container.vue
@@ -2,9 +2,17 @@
   <section>
     <h2 class="sectionHeader">My projects</h2>
     <div>
-      <card></card>
-      <card></card>
-      <card></card>
+      <card
+        v-for="(content, index) in cardContent"
+        :key="index"
+        :name="content.name"
+        :image="content.image"
+        :dSmall="content.dSmall"
+        :description="content.description"
+        :dTwo="content.dTwo"
+        :sourceLink="content.sourceLink"
+        :activeLink="content.activeLink"
+      ></card>
     </div>
   </section>
 </template>
@@ -14,6 +22,72 @@ import card from "./Card.vue";
 export default {
   components: {
     card: card
+  },
+  data() {
+    return {
+      cardContent: [
+        {
+          id: "1",
+          name: "Water Commercial",
+          dSmall: "School commercial project",
+          image: require("@/assets/commercial.jpeg"),
+          description:
+            "I was given the freedom to chose the subject and make a 20 sec commercial about it, I wasn't alone, we chose to do a video commercial about water and it's importance in our daily life.",
+          dTwo:
+            "I took the roles of main actor and director, and the focus was to communicate how important water is out everyday life, how it is part of all of our activities from dusk till dawn.",
+          sourceLink: "https://www.youtube.com/watch?v=5NYV_mr4IfM",
+          activeLink: "https://www.youtube.com/watch?v=5NYV_mr4IfM"
+        },
+        {
+          id: "2",
+          name: "EASV Canteen website",
+          dSmall: "Communcication inteface",
+          image: require("@/assets/EASV-canteen-project.jpeg"),
+          description:
+            "After evaluating the EASV Canteen and talking to the customers we found that the biggest pains are long ques and lack of information, it became clear that it was lacking a communication portal.",
+          dTwo:
+            "To resolve this we set on designing a communication interface, especially for mobile and tablet use. The goal was to inform and create a community around food and cultural diversity  The following project is the result of that.",
+          sourceLink: "https://github.com/raul-octavian/canteen-project",
+          activeLink: "easvcanteen.netlify.app"
+        },
+        {
+          id: "3",
+          name: "ToDo list from API",
+          dSmall: "Using vuex to manipulate a public API",
+          image: require("@/assets/todoList.jpg"),
+          description:
+            "The goal of this project was to learn how to use API's and Vuex state management pattern and to understand the logic behind it. ",
+          dTwo:
+            "I focused on the logic and operations so making it pretty was outside the scope. The todos are imported from a public JSON library and, the page offers the add, filter, delete and complete or incomplete state change on double-click.",
+          sourceLink: "https://github.com/raul-octavian/vuex-and-api-totolist",
+          activeLink: "https://raultodoapi.netlify.app/"
+        },
+        {
+          id: "4",
+          name: "Corona Video",
+          dSmall: "School infomercial video",
+          image: require("@/assets/corona.jpg"),
+          description:
+            "As a school team project, which allowed us to chose our subject, gave us the freedom and we chose to focus on the Corona pandemic from a quarantines person's perspective.",
+          dTwo:
+            "In this project, my role was as an animator and main actor. It was a pleasure to work with my colleagues and create something that is more than just a simple infomercial.",
+          sourceLink: "https://www.youtube.com/watch?v=T5vAHnADXMY&t",
+          activeLink: "https://www.youtube.com/watch?v=T5vAHnADXMY&t"
+        },
+        {
+          id: "5",
+          name: "Water Commercial",
+          dSmall: "School commercial project",
+          image: require("@/assets/commercial.jpeg"),
+          description:
+            "I was given the freedom to chose the subject and make a 20 sec commercial about it, I wasn't alone, we chose to do a video commercial about water and it's importance in our daily life.",
+          dTwo:
+            "I took the roles of main actor and director, and the focus was to communicate how importat water is out everyday life, how it is part of all of our activities from dusk till dawn",
+          sourceLink: "https://www.youtube.com/watch?v=5NYV_mr4IfM",
+          activeLink: "https://www.youtube.com/watch?v=5NYV_mr4IfM"
+        }
+      ]
+    };
   }
 };
 </script>
